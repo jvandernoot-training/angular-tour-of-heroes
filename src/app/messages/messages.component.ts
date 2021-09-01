743138c89d6fb1d4db47335bab94d8d9139f60a2
Jamie VanDernoot <james.vandernoot@slalom.com>
2021-09-01 13:34:49 -0500

    Added BroadcastChannel messages for POC.

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
 import { WindowServiceService } from '../services/window-service.service';
 import { MessageService } from './../services/message.service';
 
@@ -9,16 +9,32 @@ import { MessageService } from './../services/message.service';
 })
 export class MessagesComponent implements OnInit {
 
-  constructor(public messageService: MessageService, public windowService: WindowServiceService) { }
+  private broadcastChannel: BroadcastChannel;
+
+  constructor(
+    public messageService: MessageService,
+    public windowService: WindowServiceService,
+    private ngZone: NgZone) {
+      // Refer to the blog post about setting this up in a BroadcastService:
+      // https://blogs.aca-it.be/how-to-use-the-broadcastchannel-api-angular/
+    this.broadcastChannel = new BroadcastChannel("broadcast-channel-name");
+    this.broadcastChannel.onmessage = (message) => this.ngZone.run(() => messageService.add(message.data.payload));
+  }
 
   ngOnInit(): void {
   }
 
-  postMessage() {
+  broadcastMessageViaWindowService() {
     this.windowService.post();
+  }
+
+  broadcastMessageViaBroadcastChannel() {
+    let message = { type: "post", payload: "message from browser window: " + Date.now() };
+    this.broadcastChannel.postMessage(message);
   }
 
   clearMessages() {
     this.messageService.clear();
   }
+
 }
